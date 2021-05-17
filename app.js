1fa29281fa7a52ee44f311609e969502a25a9c8d
Ankit <ankit.dungriyal123@gmail.com>
2021-05-17 17:29:42 +0530

    Deploying website online using heroku web server and mongoDB database

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 
 // Connecting to mongoose database
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://<userName>:<Password>@<clusterName>.uej7s.mongodb.net/todolistDB", {useNewUrlParser : true, useUnifiedTopology: true});
 
 mongoose.set('useFindAndModify', false);
 
@@ -166,6 +166,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started at port "+ port);
 });
